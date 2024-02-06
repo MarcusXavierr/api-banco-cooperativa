c7302505b3f3d13942fc5df3cc948be09737f0e0
Marcus Xavier <marcusxavierr123@gmail.com>
2024-02-06 00:45:59 -0300

    feat: use port from env

--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	router := chi.NewRouter()
+	PORT := os.Getenv("HTTP_PORT")
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello world"))
+		w.Write([]byte("Hello darkness my old friend"))
 	})
 
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":"+PORT, router)
 }
