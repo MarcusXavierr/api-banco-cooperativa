db106fa3848ad7043698b3c34dc7852ebf4050cd
Marcus Xavier <marcusxavierr123@gmail.com>
2024-11-25 15:46:22 -0300

    feat: melhorias na validação de formulario login e registro

--- internal/auth/login.go
+++ internal/auth/login.go
@@ -3,38 +3,52 @@ package auth
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/MarcusXavierr/api-banco-cooperativa/internal/db"
+	"github.com/go-playground/validator/v10"
 	"github.com/jackc/pgx/v5"
 	"github.com/pkg/errors"
 )
 
 // TODO: Melhorar a validação de erros a nivel dos handlers
 func (service AuthService) HandleLogin(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
+	var loginRequest struct {
+		Email string `json:"email" validate:"required,email"`
+		Senha string `json:"senha" validate:"required,min=8"`
+	}
 	conn := service.DB.Conn
-	fmt.Print("oi", r.PostFormValue("password"))
-	if err != nil {
-		log.Print(time.Now(), err)
-		http.Error(w, "Could not parse post form", 500)
+	hash := sha256.New()
+
+	if err := json.NewDecoder(r.Body).Decode(&loginRequest); err != nil {
+		log.Print(err)
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
 
-	hash := sha256.New()
-	hash.Write([]byte(r.PostFormValue("password")))
+	validate := validator.New()
+	if err := validate.Struct(loginRequest); err != nil {
+		log.Print(err)
+		http.Error(w, "Validation failed\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+	fmt.Println(loginRequest)
+
+	hash.Write([]byte(loginRequest.Senha))
 
 	params := db.VerifyCredentialsParams{
-		Email:    r.PostFormValue("email"),
+		Email:    loginRequest.Email,
 		Password: hex.EncodeToString(hash.Sum(nil)),
 	}
 
 	user, err := conn.VerifyCredentials(r.Context(), params)
 	if err != nil {
-		log.Print(time.Now(), err)
+		log.Print(err)
 		if errors.Is(err, pgx.ErrNoRows) {
 			http.Error(w, "Credenciais inválidas", 401)
 		} else {
@@ -45,7 +59,7 @@ func (service AuthService) HandleLogin(w http.ResponseWriter, r *http.Request) {
 
 	token, err := service.generateTokenJWT(user)
 	if err != nil {
-		log.Print(time.Now(), err)
+		log.Print(err)
 		http.Error(w, "Error generating the jwt token", 500)
 		return
 	}
