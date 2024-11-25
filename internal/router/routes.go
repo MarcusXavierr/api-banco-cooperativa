10baf3e256508e9a3fa8dbd05cb1b706b0cf9541
Marcus Xavier <marcusxavierr123@gmail.com>
2024-11-25 14:47:23 -0300

    feat: criando rotas de autenticação

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -3,6 +3,7 @@ package router
 import (
 	"net/http"
 
+	"github.com/MarcusXavierr/api-banco-cooperativa/internal/auth"
 	"github.com/MarcusXavierr/api-banco-cooperativa/internal/db"
 	"github.com/MarcusXavierr/api-banco-cooperativa/internal/user"
 	"github.com/go-chi/chi/v5"
@@ -12,15 +13,19 @@ const UserCtxKey = "user"
 
 func initializeRoutes(router *chi.Mux, dbConn *db.DBPool) {
 	userService := user.UserService{DB: dbConn}
+	authService := auth.AuthService{DB: dbConn}
 
 	router.Route("/clientes/{id}", func(r chi.Router) {
 		r.Use(func(next http.Handler) http.Handler {
-			return fillUserDataMiddleware(next, dbConn.Conn)
+			return authenticationMiddleware(next, dbConn.Conn)
 		})
 
 		r.Get("/extrato", userService.HandleExtract)
 		r.Post("/transacoes", userService.HandleTransaction)
 	})
+
+	router.Post("/login", authService.HandleLogin)
+	router.Post("/cadastro", authService.HandleRegister)
 
 	router.Route("/teste", func(r chi.Router) {
 		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
